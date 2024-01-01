2a1abca7cbe1a6335cec4025b6c3056f456a870b
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-01 21:26:59 +0600

    Create Beecrowed 1004.cpp

---
+++ BeeCrowd/Beecrowed 1004.cpp
@@ -0,0 +1,19 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1004
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int a,b;
+    
+    cin>>a>>b;
+    
+    int PROD=a*b;
+    
+    cout<<"PROD = "<<PROD<<endl;
+    
+  return 0;  
+    
+}
