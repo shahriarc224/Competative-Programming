c2103123c8befbb2eeff262834a8cff92b512fb2
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-01 18:28:40 +0600

    Update BeeCrowd 1000.cpp

--- BeeCrowd/BeeCrowd 1000.cpp
+++ BeeCrowd/BeeCrowd 1000.cpp
@@ -1,2 +1,11 @@
-//
+//https://www.beecrowd.com.br/judge/en/problems/view/1000
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    cout<<"Hello World!"<<endl;
+    
+    return 0;
+}
