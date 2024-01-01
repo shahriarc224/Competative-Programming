3bf5441545e7e414cf80b2cf7267bed38b8a9a56
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-01 21:06:05 +0600

    Create Beecrowd 1003.cpp

---
+++ BeeCrowd/Beecrowd 1003.cpp
@@ -0,0 +1,15 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1003
+
+#include <iostream>
+using namespace std;
+int main() {
+    int A;
+    int B;
+
+    cin >> A>>B;
+    
+
+    cout << "SOMA = " << A + B <<endl;
+
+    return 0;
+}
