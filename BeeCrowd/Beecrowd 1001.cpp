3be3cc8d5a7c4b48a0b708deb3f1b93b12935283
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-01 19:29:18 +0600

    Create Beecrowd 1001.cpp

---
+++ BeeCrowd/Beecrowd 1001.cpp
@@ -0,0 +1,18 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1001
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int A, B, X;
+    
+ 
+    cout << "Enter values for A and B: ";
+    cin >> A >> B;
+
+    X = A + B;
+
+    
+    cout << "X = " << X << endl;
+
+    return 0;
+}
