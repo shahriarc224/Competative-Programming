2ad21cd7ba1b67e1944040741f5b75ce518ae3fd
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-01 19:29:46 +0600

    Create Beecrowed 1002.cpp

---
+++ BeeCrowd/Beecrowed 1002.cpp
@@ -0,0 +1,18 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1002
+
+#include<bits/stdc++.h>
+using namespace std; 
+int main(){
+    
+    double R,A;
+    
+    double k= 3.14159;
+    
+    cin>>R;
+    
+ A =  k*R*R;
+ 
+ cout<<"A="<<fixed<<setprecision(4)<<A<<endl;
+    
+    return 0;
+}
