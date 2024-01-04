83e6e7a0668df5502086add5b0ee2f4d2c114fb9
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-05 00:55:06 +0600

    Create beecrowd 1008.cpp

---
+++ BeeCrowd/beecrowd 1008.cpp
@@ -0,0 +1,17 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1008
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    float c;
+
+    cin>>a>>b>>c;
+    
+  float d =b*c;
+    
+    cout<<"NUMBER = "<<a<<endl<<"SALARY = U$ "<<fixed<<setprecision(2)<<d<<endl;
+    
+    
+}
