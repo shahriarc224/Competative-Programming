9c2363e3f90411854d7b7ac4e559bb3c6f4a34e6
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-05 00:33:07 +0600

    Create beecrowd 1006.cpp

---
+++ BeeCrowd/beecrowd 1006.cpp
@@ -0,0 +1,16 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1006
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+   double A,B,C;
+   
+      cin>>A>>B>>C;
+   
+     double media=(A*2+B*3+C*5)/(2+3+5);
+    
+    
+    cout<<"MEDIA = "<<fixed<<setprecision(1)<<media<<endl;
+return 0;    
+}
