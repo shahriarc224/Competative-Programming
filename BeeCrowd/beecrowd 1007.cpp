754099f3999abc52440e73b43cee6f38c7c40d68
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-05 00:44:12 +0600

    Create beecrowd 1007.cpp

---
+++ BeeCrowd/beecrowd 1007.cpp
@@ -0,0 +1,17 @@
+///https://www.beecrowd.com.br/judge/en/problems/view/1007
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int A,B,C,D;
+    
+cin>>A>>B>>C>>D;
+
+int diferenca = (A * B - C * D);
+
+cout<<"DIFERENCA = "<<diferenca<<endl;
+    
+    
+}
