84d139e9ad63564e9f9c6ec8c8e32552d8ef9994
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-07 23:53:48 +0600

    Create BeeCrowd 1009.cpp

---
+++ BeeCrowd/BeeCrowd 1009.cpp
@@ -0,0 +1,18 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1009
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string a;
+    cin>>a;
+
+    double b,c;
+    cin>>b>>c;
+    double total =(((15*c)/100)+b);
+    
+    cout<<"TOTAL = R$ "<<fixed<<setprecision(2)<<total<<endl;
+    
+    
+    return 0;
+}
