2c238f4eba0a326dba477aa8ee8c57f96146f132
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-08 22:21:09 +0600

    Create Beecrowed 1011.cpp

---
+++ BeeCrowd/Beecrowed 1011.cpp
@@ -0,0 +1,19 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1011
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int R;
+    cin>>R;
+    double pi=3.14159;
+    
+    double volume=(4.0/3)*pi*pow(R,3);
+    
+    cout<<"VOLUME = "<<fixed<<setprecision(3)<<volume<<endl;
+    
+}
+    
+    
+    
+    
