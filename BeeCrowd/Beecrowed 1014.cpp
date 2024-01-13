27b55811968be3677530d7ebe120f74ff60702ea
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-13 10:42:30 +0600

    Create Beecrowed 1014.cpp

---
+++ BeeCrowd/Beecrowed 1014.cpp
@@ -0,0 +1,18 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1014
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+    float x,y;
+
+    cin>>x;
+    cin>>y;
+
+     float a=(x/y);
+
+    cout<<fixed<<setprecision(3)<<a<<" km/l"<<endl;
+
+}
