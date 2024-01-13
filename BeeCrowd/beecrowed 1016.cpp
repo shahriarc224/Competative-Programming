bdc878dfdfd2a4a519a08e2d1adacd64a77e1588
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-13 19:35:44 +0600

    Create beecrowed 1016.cpp

---
+++ BeeCrowd/beecrowed 1016.cpp
@@ -0,0 +1,13 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1016
+
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+
+    int a;
+    cin>>a;
+
+    cout<<a*2<<" minutos"<<endl;
+
+}
