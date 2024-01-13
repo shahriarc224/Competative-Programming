841912feaa02c787f84e90fb13b0ffb4908f0418
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-13 19:59:26 +0600

    Create Bee crowed 1017.cpp

---
+++ BeeCrowd/Bee crowed 1017.cpp
@@ -0,0 +1,14 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1017
+
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+
+    float a,b;
+    cin>>a>>b;
+ float c=(a*b)/12;
+
+    cout<<fixed<<setprecision(3)<<c<<endl;
+}
+
