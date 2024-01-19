bd1b9f6c721ea074ec83a43afe34dbd67c69f44b
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-19 20:00:15 +0600

    Create Beecroweed1021.cpp

---
+++ BeeCrowd/Beecroweed1021.cpp
@@ -0,0 +1,16 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1021
+
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+int main(){
+    
+    double a;
+    cin>>a;
+    
+double b=a/100;
+
+    
+    
+}
