665925d607f9ca200c1980f1cae746d413049929
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-23 19:25:09 +0600

    Create Beecrowed 1047.cpp

---
+++ BeeCrowd/Beecrowed 1047.cpp
@@ -0,0 +1,46 @@
+
+/*
+ai code ta pari nai 
+source code link :
+
+
+code:
+
+
+
+
+*/
+
+
+
+// ai code ta hoy nai 
+//https://www.beecrowd.com.br/judge/en/problems/view/1046
+
+#include<bits/stdc++.h>
+using namespace std ;
+int main(){
+    
+    /*hour a,c
+    minutes b.d
+    
+    */
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    
+    if(a>12 || c<12 && b>60 ||d<60 ){
+        
+        int x=(24-a)+c;
+        int g=(60-b)+d;
+        cout<<"O JOGO DUROU "<<x<<" HORA(S) E "<<g<<" MINUTO(S)"<<endl;
+        
+    }
+    else if(a<12 || c>12 && b<60 ||d>60) {
+        
+     int z=c-a;
+     
+     int h=d-b;
+        cout<<"O JOGO DUROU "<<z<<" HORA(S) E "<<h<<"MINUTO(S)"<<endl;
+
+    }
+    
+}
