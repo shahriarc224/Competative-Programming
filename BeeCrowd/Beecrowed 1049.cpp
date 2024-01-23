bb3d086fa57534874c61c86ec6ca82fa9840c69a
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-23 22:26:03 +0600

    Update Beecrowed 1049.cpp

--- BeeCrowd/Beecrowed 1049.cpp
+++ BeeCrowd/Beecrowed 1049.cpp
@@ -1,3 +1,69 @@
+/*
+
+source code:   https://www.codeshikhi.com/2020/12/uri-1049-animal-solution-in-c-cpp-cpluscplus-python.html
+
+*/
+
+// code:
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+ string c;
+
+ cin >> c;
+
+ if(c == "vertebrado"){
+  
+  cin >> c;
+  if(c == "ave"){
+
+   cin >> c;
+   if(c == "carnivoro"){
+    cout << "aguia" << endl;
+   }else{
+    cout << "pomba" << endl;
+   }
+  }else{
+
+   cin >> c;
+   if(c == "onivoro"){
+    cout << "homem" << endl;
+   }else{
+    cout << "vaca" << endl;
+   }
+  }
+ }else{
+
+  cin >> c;
+  if(c == "inseto"){
+
+   cin >> c;
+   if(c == "hematofago"){
+    cout << "pulga" << endl;
+   }else{
+    cout << "lagarta" << endl;
+   }
+  }else{
+
+   cin >> c;
+   if(c == "hematofago"){
+    cout << "sanguessuga" << endl; 
+   }else{
+    cout << "minhoca" << endl;
+   }
+  }
+ }
+ return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// i dont know why this code is not working properly 
+
 //https://www.beecrowd.com.br/judge/en/problems/view/1049
 
 #include<bits/stdc++.h>
