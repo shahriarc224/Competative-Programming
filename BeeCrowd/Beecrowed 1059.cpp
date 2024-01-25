b1a0503368ca0fdee0269fbe04d359587a19ad31
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-01-25 12:42:24 +0600

    Create Beecrowed 1059.cpp

---
+++ BeeCrowd/Beecrowed 1059.cpp
@@ -0,0 +1,19 @@
+//  https://www.beecrowd.com.br/judge/en/problems/view/1059
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+for(int i=2;i<=100;i+=2){
+
+
+cout<<i<<endl;
+
+}
+
+}
+
+
