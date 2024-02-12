fb477f8ab86c66264ae65448934dcaa0dd401114
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-12 23:01:22 +0600

    Create Beecrowed 1072.cpp

---
+++ BeeCrowd/Beecrowed 1072.cpp
@@ -0,0 +1,48 @@
+ /*
+
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://judge.beecrowd.com/en/problems/view/1072
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int cnt1=0;
+int cnt2=0;
+int a;
+int t;
+cin>>t;
+while(t--){
+cin>>a;
+    if(a<=20 && a>=10){
+
+            cnt1++;
+
+    }
+    else{
+
+        cnt2++;
+
+    }
+
+
+}
+
+cout<<cnt1<<" in"<<endl;
+cout<<cnt2<<" out"<<endl;
+
+
+}
+
