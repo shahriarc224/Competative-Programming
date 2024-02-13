17b4126a2d8f391b91c9e4e78ba08360efeb1b1d
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-13 18:41:39 +0600

    Create Beecrowed 1079.cpp

---
+++ BeeCrowd/Beecrowed 1079.cpp
@@ -0,0 +1,24 @@
+// https://judge.beecrowd.com/en/problems/view/1079
+
+// aita pari nai 
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    float a, b, c;
+    
+    cin >> n;
+    
+    for(int i = 0; i < n; i++){
+            cin >> a;
+            cin >> b;
+            cin >> c;
+            
+            printf("%.1f\n", (a*2 + b*3 + c*5)/(2.0+3.0+5.0));
+    }
+    
+    return 0;
+}
