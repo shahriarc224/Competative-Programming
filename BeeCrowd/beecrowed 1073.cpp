7d09af82c17379ac31f224b18f5ed644af52e4c9
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-13 12:59:19 +0600

    Create beecrowed 1073.cpp

---
+++ BeeCrowd/beecrowed 1073.cpp
@@ -0,0 +1,20 @@
+// https://judge.beecrowd.com/en/problems/view/1073
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int n;
+    cin>>n;
+    
+    for(int i=1;i<=n;i++){
+    if(i%2==0){
+        
+        cout<<i<<"^2 = "<<i*i<<endl;
+        
+    }    
+    
+        
+    }
+    
+}
