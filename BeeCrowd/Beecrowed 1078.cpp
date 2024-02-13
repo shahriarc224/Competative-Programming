5dfd0cd822b5b0ba88aece46f90885304f852f6d
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-13 18:40:59 +0600

    Create Beecrowed 1078.cpp

---
+++ BeeCrowd/Beecrowed 1078.cpp
@@ -0,0 +1,17 @@
+// https://judge.beecrowd.com/en/problems/view/1078
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int n;
+    cin>>n;
+    for(int i=1;i<=10;i++){
+        
+      cout<<i<<" x "<<n<<" = "<<i*n<<endl;
+        
+    }
+    
+}
+
+
