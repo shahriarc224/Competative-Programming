397e7e46a301b6d1cbeffcc2737e063c0696f993
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-13 18:40:29 +0600

    Create Beeceowed 1075.cpp

---
+++ BeeCrowd/Beeceowed 1075.cpp
@@ -0,0 +1,21 @@
+// https://judge.beecrowd.com/en/problems/view/1075
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int n;
+    cin>>n;
+    
+    for(int i=1;i<=10000;i++){
+        
+        if(i%n==2){
+            
+            cout<<i<<endl;
+        }
+        
+    }
+    
+    
+}
+
