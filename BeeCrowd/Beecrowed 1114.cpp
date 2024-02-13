d42b6e68ef4a7c3ff963645e0d8fa061a128b1f0
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-13 19:23:48 +0600

    Create Beecrowed 1114.cpp

---
+++ BeeCrowd/Beecrowed 1114.cpp
@@ -0,0 +1,26 @@
+// https://judge.beecrowd.com/en/problems/view/1114
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+   while(1){
+
+        int a;
+    cin>>a;
+    if(a!=2002){
+
+            cout<<"Senha Invalida"<<endl;
+        }
+      else {
+
+        cout<<"Acesso Permitido"<<endl;
+
+        return 0;
+
+
+   }
+
+}
+}
+
