323053713359b9def68300d65ecc5f3140599b1e
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-15 21:15:34 +0600

    Create Beecrowed 1143.cpp

---
+++ BeeCrowd/Beecrowed 1143.cpp
@@ -0,0 +1,35 @@
+ /*
+
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://judge.beecrowd.com/en/problems/view/1143
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int n;
+cin>>n;
+
+for(int i=1;i<=n;i++){
+
+    cout<<i<<" "<<pow(i,2)<<" "<<pow(i,3)<<endl;
+
+}
+
+
+
+
+}
+
