e199ec984c592a6a7ba552683d6fd0a8d56263ed
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-17 17:19:49 +0600

    Create Beecrowd 1096.cpp

---
+++ BeeCrowd/Beecrowd 1096.cpp
@@ -0,0 +1,19 @@
+// https://judge.beecrowd.com/en/problems/view/1096
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+   for(int i=1;i<10;i+=2){
+       
+       for(int j=7;j>4;j--){
+           
+           cout<<"I="<<i<<" "<<"J="<<j<<endl;
+           
+       }
+       
+   }
+
+    
+    
+}
