fdf7ba836934d704168209fda71947d2f8c69454
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-17 09:12:38 +0600

    Create beecrowed 1040.cpp

---
+++ BeeCrowd/beecrowed 1040.cpp
@@ -0,0 +1,42 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1040
+
+/*ai problem tar solve pari nai 
+
+source link:    https://www.codeshikhi.com/2020/12/uri-1040-average-3-solution-in-c-cpp-cpluscplus-python.html
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double first, second, third, fourth, last, sum;
+    cin>>first>>second>>third>>fourth;
+    sum = (first * 2 + second * 3 + third * 4 + fourth) / 10;
+    cout<<fixed;
+    cout<<setprecision(1)<<"Media: "<<sum<<endl;
+    if (sum >= 7.0){
+        cout<<"Aluno aprovado.\n";
+    }
+    else if (sum >= 5.0)
+    {
+        cout<<"Aluno em exame.\n";
+        cin>>last;
+       
+        cout<<setprecision(1)<<"Nota do exame: "<<last<<endl;
+        if (last + sum / 2.0 > 5.0){
+            cout<<"Aluno aprovado.\n";
+        }
+        else{
+            cout<<"Aluno reprovado.\n";
+        }
+        
+        cout<<setprecision(1)<<"Media final: "<<(last + sum ) / 2.0<<endl;
+    }
+    else{
+        cout<<"Aluno reprovado.\n";
+    }
+    return 0;
+}
