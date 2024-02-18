32d2053f31a8efe67cc924523e207ba741fafd11
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-18 19:35:03 +0600

    Create Beecrowed 1759.cpp

---
+++ BeeCrowd/Beecrowed 1759.cpp
@@ -0,0 +1,21 @@
+// https://judge.beecrowd.com/en/problems/view/1759
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int n;
+cin>>n;
+
+for(int i=1;i<=n-1;i++){
+
+	cout<<"Ho ";
+
+}
+	cout<<"Ho!"<<endl;
+
+
+}
+
+
+
