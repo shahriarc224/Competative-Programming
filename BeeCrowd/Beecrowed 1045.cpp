a666bf3110a1fe749c56ca8b638a773a10fb6a0f
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-18 11:38:22 +0600

    Update Beecrowed 1045.cpp

--- BeeCrowd/Beecrowed 1045.cpp
+++ BeeCrowd/Beecrowed 1045.cpp
@@ -1,63 +1,75 @@
-/* ai code ta pari nai 
+// https://judge.beecrowd.com/en/problems/view/1045
 
-source code:
+//ai problem ta pari nai 
 
-code:
-
-
-
-
-
-
-*/
-
-
-
-
-//nicer ta vul code 
-
-
-//https://www.beecrowd.com.br/judge/en/problems/view/1045
+// source link : https://www.codeshikhi.com/2020/12/uri-1045-triangle-types-solution-in-c-cpp-cpluscplus-python.html
 
 #include<bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
+int main(){
+    double a,b,c;
+
+    cin >> a >> b >> c;
+
+    if(a >= (b+c) || b >= (a+c) || c >= (a+b))
+         cout << "NAO FORMA TRIANGULO"<<endl;
+    else if(a*a == (b*b + c*c) || b*b == (a*a + c*c) || c*c == (a*a + b*b))
+           cout << "TRIANGULO RETANGULO"<<endl;
+    else if(a*a > (b*b + c*c) || b*b > (a*a + c*c) || c*c > (a*a + b*b))
+           cout << "TRIANGULO OBTUSANGULO"<<endl;
+    else if(a*a < (b*b + c*c) || b*b < (a*a + c*c) || c*c < (a*a + b*b))
+           cout << "TRIANGULO ACUTANGULO"<<endl;
+    if(a == b && a == c)
+            cout << "TRIANGULO EQUILATERO"<<endl;
+    if((a == b && a != c)  || (a == c && a != b) || (b == c && b != a))
+            cout << "TRIANGULO ISOSCELES"<<endl;
+
+return 0;
+    
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// My code :
+
+        
+#include<bits/stdc++.h>
+using namespace std;
 int main()
-
 {
-    
-   double A,B,C;
-    
-    cin>>A>>B>>C;
-    
-    
-   if(A >=B + C){
-       
-       cout<<"NAO FORMA TRIANGULO"<<endl;
-       
-   }if((A*A)==(B*B) + (C*C)){
-       
-       cout<<"TRIANGULO RETANGULO"<<endl;
-   
-}if((A*A)>(B*B)+(C*C)){
-       
-       cout<<"TRIANGULO OBTUSANGULO"<<endl;
-   
-}if((A*A)<(B*B)+(C*C)){
-       
-       cout<<"TRIANGULO ACUTANGULO"<<endl;
-   
-}if(A==B==C){
-       
-       cout<<"TRIANGULO EQUILATERO"<<endl;
-       
-   
-}if(A==B || B==C ||C==A){
-       
-       cout<<"TRIANGULO ISOSCELES"<<endl;
-   
-}
-
-
+    double a,b,c;
+    cin>>a>>b>>c;
+    if(a >= (b+c))
+    {
+        cout<<"NAO FORMA TRIANGULO"<<endl;
+    }
+    if(a*a == (b*b+c*c))
+    {
+        cout<<"TRIANGULO RETANGULO"<<endl;
+    }
+    if(a*a > (b*b + c*c) || b*b > (a*a + c*c) || c*c > (a*a + b*b))
+    {
+        cout<<"TRIANGULO OBTUSANGULO"<<endl;
+    }
+    if(a*a < (b*b + c*c) || b*b < (a*a + c*c) || c*c < (a*a + b*b))
+    {
+        cout<<"TRIANGULO ACUTANGULO"<<endl;
+    }
+    if(a == b && b == c)
+    {
+        cout<<"TRIANGULO EQUILATERO"<<endl;
+    }
+    if(a == b || b == c ||a == c )
+    {
+        cout<<"TRIANGULO ISOSCELES"<<endl;
+    }
 
 }
+
+
+
+
+
+
+        
+        
