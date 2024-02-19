6756af8efc0a4e7aed9f3b716d60a15e16bf35f7
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-19 20:39:27 +0600

    Create Beecrowed 1030.cpp

---
+++ BeeCrowd/Beecrowed 1030.cpp
@@ -0,0 +1,15 @@
+// https://judge.beecrowd.com/en/problems/view/1930
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int a,b,c,d;
+cin>>a>>b>>c>>d;
+
+int e=a+b+c+d;
+
+cout<<e-3<<endl;
+
+
+}
