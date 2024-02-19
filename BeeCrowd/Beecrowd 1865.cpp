cd53219000e7ecaf892a1d2a5f5a39f46d8fe625
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-19 20:21:32 +0600

    Create Beecrowd 1865.cpp

---
+++ BeeCrowd/Beecrowd 1865.cpp
@@ -0,0 +1,14 @@
+// https://judge.beecrowd.com/en/problems/view/1865
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+   string a;
+   int b;
+   cin>>a>>b;
+   cout<<((a=="Thor")?"Y":"N")<<endl;
+}
+}
