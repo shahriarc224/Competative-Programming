f51274d54bab6d63d43427c15160875dbd9aa5f0
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-19 20:45:32 +0600

    Create Beecrowd 1933.cpp

---
+++ BeeCrowd/Beecrowd 1933.cpp
@@ -0,0 +1,19 @@
+// https://judge.beecrowd.com/en/problems/view/1933
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a,b;
+cin>>a>>b;
+if(a>b){
+
+    cout<<a<<endl;;
+}else if(b>a){
+
+cout<<b<<endl;
+
+}else if(a==b){
+cout<<a<<endl;
+}
+
+}
