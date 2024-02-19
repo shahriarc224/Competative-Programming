7f0ce81f4ed7d0d101ad69282fdd832ad1a95d18
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-19 19:57:58 +0600

    Create Beecrowd 1866.cpp

---
+++ BeeCrowd/Beecrowd 1866.cpp
@@ -0,0 +1,37 @@
+ /*
+
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://judge.beecrowd.com/en/problems/view/1866
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+
+    int a;
+    cin>>a;
+
+   if(a%2==0){
+   cout<<"0"<<endl;
+   } else{
+
+   cout<<"1"<<endl;
+   }
+
+}
+}
