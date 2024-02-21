58fb16018d7af2f2fd54d3787073b505a58851ae
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-21 17:44:52 +0600

    Create Beecrowd 2581.cpp

---
+++ BeeCrowd/Beecrowd 2581.cpp
@@ -0,0 +1,35 @@
+ /*
+
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://judge.beecrowd.com/en/problems/view/2581
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+
+    string a;
+    cin>>a;
+
+    cout<<"I am Toorg!"<<endl;
+
+}
+
+}
+
+
