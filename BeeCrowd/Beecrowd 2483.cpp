eae0ebf8397963f8957a2707c844ee1afd061ea2
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-22 13:18:31 +0600

    Create Beecrowd 2483.cpp

---
+++ BeeCrowd/Beecrowd 2483.cpp
@@ -0,0 +1,33 @@
+ /*
+
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://judge.beecrowd.com/en/problems/view/2483
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int a;
+cin>>a;
+
+cout<<"Feliz nat";
+for(int i=0;i<a;i++){
+
+    cout<<"a";
+}
+cout<<"l!"<<endl;
+
+
+}
+
