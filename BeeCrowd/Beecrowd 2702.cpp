4b4ff0463322c30bc0528bb5bae43d3508796a50
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-22 19:12:41 +0600

    Create Beecrowd 2702.cpp

---
+++ BeeCrowd/Beecrowd 2702.cpp
@@ -0,0 +1,47 @@
+ /*
+
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://judge.beecrowd.com/en/problems/view/2702
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+int a,b,c;
+cin>>a>>b>>c;
+int d,e,f;
+cin>>d>>e>>f;
+int g=0,h=0,i=0,j=0;
+if(a<d){
+
+    g=d-a;
+
+} if(b<e){
+
+h=e-b;
+
+
+}if(c<f){
+
+i=f-c;
+
+}
+j=g+h+i;
+
+cout<<j<<endl;
+
+}
+
