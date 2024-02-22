c8224630cdd6ee24463f92884674fc769eb71779
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-22 21:51:42 +0600

    Create Beecrowd 2752.cpp

---
+++ BeeCrowd/Beecrowd 2752.cpp
@@ -0,0 +1,34 @@
+ /*
+
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://judge.beecrowd.com/en/problems/view/2752
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+printf("<AMO FAZER EXERCICIO NO URI>\n");
+printf("<    AMO FAZER EXERCICIO NO URI>\n");
+printf("<AMO FAZER EXERCICIO >\n");
+printf("<AMO FAZER EXERCICIO NO URI>\n");
+printf("<AMO FAZER EXERCICIO NO URI    >\n");
+printf("<AMO FAZER EXERCICIO NO URI>\n");
+printf("<          AMO FAZER EXERCICIO >\n");
+printf("<AMO FAZER EXERCICIO           >\n");
+
+
+}
+
