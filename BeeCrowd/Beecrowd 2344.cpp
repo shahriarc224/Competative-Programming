e44b9d9a6c2c4fed8f10cdc5bc96250f3d9a6f69
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-22 13:10:51 +0600

    Create Beecrowd 2344.cpp

---
+++ BeeCrowd/Beecrowd 2344.cpp
@@ -0,0 +1,45 @@
+ /*
+
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://judge.beecrowd.com/en/problems/view/2344
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int a;
+cin>>a;
+
+if(a==0){
+    cout<<"E"<<endl;
+
+}else if(a=>1 && a<=35){
+
+cout<<"D"<<endl;
+
+}else if(a>=36 && a<=60){
+
+cout<<"C"<<endl;
+}else if(a>=61 && a<=85){
+cout<<"B"<<endl;
+
+}else if(a>=86 && a<=100 ){
+
+cout<<"A"<<endl;
+}
+
+}
+
+
