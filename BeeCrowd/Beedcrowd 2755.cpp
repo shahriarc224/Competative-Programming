cc82617a53657d893eed37e0a670004b6ddfb5cb
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-22 22:12:20 +0600

    Create Beedcrowd 2755.cpp

---
+++ BeeCrowd/Beedcrowd 2755.cpp
@@ -0,0 +1,18 @@
+// ai code ta te compilatoion error dekhay 
+// source link : https://devsenv.com/example/-2755-beecrowd-online-judge-solution-2755-output-9-solution-in-c,-c++,-java,-js-and-python
+
+// code
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\"Ro'b'er\tto\\/\"" << endl;
+    cout << "(._.) ( l: ) ( .-. ) ( :l ) (._.)" << endl;
+    cout << "(^_-) (-_-) (-_^)" << endl;
+    cout << "(\"_\") ('.')" << endl;
+    return 0;
+}
+
+//.......................................................................................................................
