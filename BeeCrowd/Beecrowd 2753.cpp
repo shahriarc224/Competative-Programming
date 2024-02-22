7fa0379d986898b919d979865b0fd3fba66830c0
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-22 21:57:48 +0600

    Create Beecrowd 2753.cpp

---
+++ BeeCrowd/Beecrowd 2753.cpp
@@ -0,0 +1,50 @@
+ /*
+
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://judge.beecrowd.com/en/problems/view/2753
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+printf("97 e a\n");
+printf("98 e b\n");
+printf("99 e c\n");
+printf("100 e d\n");
+printf("101 e e\n");
+printf("102 e f\n");
+printf("103 e g\n");
+printf("104 e h\n");
+printf("105 e i\n");
+printf("106 e j\n");
+printf("107 e k\n");
+printf("108 e l\n");
+printf("109 e m\n");
+printf("110 e n\n");
+printf("111 e o\n");
+printf("112 e p\n");
+printf("113 e q\n");
+printf("114 e r\n");
+printf("115 e s\n");
+printf("116 e t\n");
+printf("117 e u\n");
+printf("118 e v\n");
+printf("119 e w\n");
+printf("120 e x\n");
+printf("121 e y\n");
+printf("122 e z\n");
+
+}
+
