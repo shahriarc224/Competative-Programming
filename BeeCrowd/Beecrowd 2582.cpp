e2e2f4f317b7b8c9ab41bffd16b8897a956b6810
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-22 17:00:30 +0600

    Create Beecrowd 2582.cpp

---
+++ BeeCrowd/Beecrowd 2582.cpp
@@ -0,0 +1,80 @@
+ /*
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://judge.beecrowd.com/en/problems/view/2582
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+    int a,b;
+    int button=0;
+    cin>>a>>b;
+
+    button=a+b;
+
+    if(button==0){
+
+        cout<<"PROXYCITY"<<endl;
+    }
+
+
+else if (button==1){
+
+        cout<<"P.Y.N.G."<<endl;
+    }
+    else if(button==2){
+
+        cout<<"DNSUEY!"<<endl;
+    }
+    else if(button==3){
+
+        cout<<"SERVERS"<<endl;
+    }
+    else if(button==4){
+
+        cout<<"HOST!"<<endl;
+    }
+    else if(button==5){
+
+        cout<<"CRIPTONIZE"<<endl;
+    }
+    else if(button==6){
+
+        cout<<"OFFLINE DAY"<<endl;
+    }
+    else if(button==7){
+
+        cout<<"SALT"<<endl;
+    }
+    else if(button==8){
+
+        cout<<"ANSWER!"<<endl;
+    }
+    else if(button==9){
+
+        cout<<"RAR?"<<endl;
+    }
+    else if(button==10){
+
+        cout<<"WIFI ANTENNAS"<<endl;
+    }
+}
+
+}
+
+
