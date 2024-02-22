f73656dbb8ec690cb9256596415f5496042147d7
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-22 22:04:36 +0600

    Create Beecrowd 2754.cpp

---
+++ BeeCrowd/Beecrowd 2754.cpp
@@ -0,0 +1,33 @@
+ /*
+
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://judge.beecrowd.com/en/problems/view/2754
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+printf("234.345000 - 45.698000\n");
+printf("234 - 46\n");
+printf("234.3 - 45.7\n");
+printf("234.34 - 45.70\n");
+printf("234.345 - 45.698\n");
+printf("2.343450e+02 - 4.569800e+01\n");
+printf("2.343450E+02 - 4.569800E+01\n");
+printf("234.345 - 45.698\n");
+printf("234.345 - 45.698\n");
+
+}
+
