cd2837ee9f21300c56e8b9cd624fa2271469da65
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-23 10:29:09 +0600

    Create Beecrowd 2760.cpp

---
+++ BeeCrowd/Beecrowd 2760.cpp
@@ -0,0 +1,33 @@
+ /*
+
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://judge.beecrowd.com/en/problems/view/2760
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+string a,b,c;
+getline(cin,a);
+getline(cin,b);
+getline(cin,c);
+
+cout<<a<<b<<c<<endl;
+cout<<b<<c<<a<<endl;
+cout<<c<<a<<b<<endl;
+cout << a.substr(0, 10) << b.substr(0, 10) << c.substr(0, 10) << endl;
+
+}
+
