832357a45667cd0e4abf682122ddd93dbea333e9
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-23 20:17:59 +0600

    Create Beecrowd 3146.cpp

---
+++ BeeCrowd/Beecrowd 3146.cpp
@@ -0,0 +1,15 @@
+// https://judge.beecrowd.com/en/problems/view/3146
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+float a,b=0;
+cin>>a;
+
+b=2*3.14*a;
+
+cout<<fixed<<setprecision(2)<<b<<endl;
+
+
+}
