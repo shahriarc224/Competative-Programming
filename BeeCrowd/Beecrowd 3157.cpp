9980465e41cd248e0137f87e6fdb3b9e496aa8b6
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-24 21:13:40 +0600

    Create Beecrowd 3157.cpp

---
+++ BeeCrowd/Beecrowd 3157.cpp
@@ -0,0 +1,9 @@
+// https://judge.beecrowd.com/en/problems/view/3091
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+string a;
+getline(cin,a);
+cout<<"Nao se refuta ciencia com opiniao"<<endl;
+}
