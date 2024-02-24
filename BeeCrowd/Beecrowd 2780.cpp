3bb6cabe6252837f216d20fb4b2a9edb6a783d76
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-24 20:38:45 +0600

    Create Beecrowd 2780.cpp

---
+++ BeeCrowd/Beecrowd 2780.cpp
@@ -0,0 +1,11 @@
+// https://judge.beecrowd.com/en/problems/view/2780
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+if(n<=800)cout<<"1"<<endl;
+else if(n>800 && n<=1400)cout<<"2"<<endl;
+else if(n>1400 && n<= 2000) cout<<"3"<<endl;
+}
