4bbe752a2559b01ff77d0cf9491a4bc5eb797d80
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-24 22:20:56 +0600

    Create Beecrowd 3046.cpp

---
+++ BeeCrowd/Beecrowd 3046.cpp
@@ -0,0 +1,14 @@
+// https://judge.beecrowd.com/en/problems/view/3046
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int a,b=0;
+cin>>a;
+
+b=(((a+1)*(a+2))/2);
+
+cout<<b<<endl;
+
+}
