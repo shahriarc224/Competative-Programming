c0d59511648a86eac7ff2b0512290054163677a8
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-24 21:28:45 +0600

    Create Beecrowd 3040.cpp

---
+++ BeeCrowd/Beecrowd 3040.cpp
@@ -0,0 +1,22 @@
+// https://judge.beecrowd.com/en/problems/view/3040
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+int a,b,c;
+cin>>a>>b>>c;
+        if(a>=200 && a<=300 && b>=50 && c>=150 ){
+
+            cout<<"Sim"<<endl;
+        }else {
+
+        cout<<"Nao"<<endl;
+        }
+    }
+}
+
+
