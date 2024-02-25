f8f9424d7a4abb68bf2a6f3e4acdd12fa2010c39
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-25 17:06:43 +0600

    Create Beecrowd 3301.cpp

---
+++ BeeCrowd/Beecrowd 3301.cpp
@@ -0,0 +1,16 @@
+//  https://judge.beecrowd.com/en/problems/view/3301
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int a,b,c;
+cin>>a>>b>>c;
+
+           if(b==6)cout<<"zezinho"<<endl;
+         if(b==5)cout<<"luisinho"<<endl;
+         if(b==7)cout<<"huguinho"<<endl;
+
+}
+
+
