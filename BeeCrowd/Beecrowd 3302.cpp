203127d6f9f7d4c7135c85be4cc0a472687fd5b6
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-25 17:18:25 +0600

    Create Beecrowd 3302.cpp

---
+++ BeeCrowd/Beecrowd 3302.cpp
@@ -0,0 +1,17 @@
+//  https://judge.beecrowd.com/en/problems/view/3302
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t,i=0;
+cin>>t;
+while(t--){
+int a;
+    cin>>a;
+    i++;
+ cout<<"resposta "<<i<<": "<<a<<endl;
+
+}
+}
+
+
