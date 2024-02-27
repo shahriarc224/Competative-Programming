2637f90cde331a88347d30f65c64bc95efd909b1
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-27 15:44:10 +0600

    Create Beecrowd 1113.cpp

---
+++ BeeCrowd/Beecrowd 1113.cpp
@@ -0,0 +1,32 @@
+// https://judge.beecrowd.com/en/problems/view/1113
+
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+
+    while(1)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(a==b)
+        {
+            break;
+
+        }
+        else
+        {
+
+            if(a<b) cout<<"Crescente"<<endl;
+
+            else cout<<"Decrescente"<<endl;
+
+
+
+        }
+
+
+    }
+
+
+}
