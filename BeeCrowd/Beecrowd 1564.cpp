2fc4c952080d492843ef30b89dff877907a28f4b
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-29 19:45:48 +0600

    Create Beecrowd 1564.cpp

---
+++ BeeCrowd/Beecrowd 1564.cpp
@@ -0,0 +1,22 @@
+// https://judge.beecrowd.com/en/problems/view/1564
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    while(scanf("%d",&a)!=EOF)
+    {
+
+        if(a==0)
+        {
+            cout<<"vai ter copa!"<<endl;
+        }
+        else if(a>0)
+        {
+            cout<<"vai ter duas!"<<endl;
+        }
+
+    }
+
+}
