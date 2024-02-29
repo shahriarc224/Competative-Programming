40568dfef7a7c0b2b60182f1c54271ef59dda55c
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-29 21:46:39 +0600

    Create Becrowd 2936.cpp

---
+++ BeeCrowd/Becrowd 2936.cpp
@@ -0,0 +1,20 @@
+//  https://judge.beecrowd.com/en/problems/view/2936
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int a,b,c,d,e,f;
+cin>>a>>b>>c>>d>>e;
+
+int g=0,h=0,i=0,j=0,k=0;
+
+g=a*300;
+h=b*1500;
+i=c*600;
+j=d*1000;
+k=e*150;
+
+cout<<g+h+i+j+k+225<<endl;
+
+}
