31e7196d4e0aa4ffedf5c070f4b14db76f8b04c9
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-02-29 20:50:17 +0600

    Create Beecrowd 2172.cpp

---
+++ BeeCrowd/Beecrowd 2172.cpp
@@ -0,0 +1,26 @@
+//  https://judge.beecrowd.com/en/problems/view/2172
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long a,b;
+
+    while(1)
+    {
+
+        cin>>a>>b;
+        if(a==0 && b==0)
+        {
+            break;
+        }
+        else
+        {
+
+            cout<<a*b<<endl;
+        }
+
+    }
+
+
+}
