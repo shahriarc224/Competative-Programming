bfa25b64d71a51be068311f38180c18ec1ccf92f
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-01 12:07:57 +0600

    Create Beecrowd 2862.cpp

---
+++ BeeCrowd/Beecrowd 2862.cpp
@@ -0,0 +1,18 @@
+//  https://judge.beecrowd.com/en/problems/view/2862
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+    int a;
+    cin>>a;
+    if(a>8000)cout<<"Mais de 8000!"<<endl;
+    else cout<<"Inseto!"<<endl;
+
+
+}
+
+}
