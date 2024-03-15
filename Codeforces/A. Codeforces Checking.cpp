5a0c8e69dbd1c822bd0fedf45b56e572985aec44
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-15 16:58:45 +0600

    Create A. Codeforces Checking.cpp

---
+++ Codeforces/A. Codeforces Checking.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/contest/1791/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+  int t;
+  cin>>t;
+      string a="codeforces";
+
+  while(t--){
+
+   char b;
+   cin>>b;
+
+if (a.find(b) != string::npos) cout<<"YES"<<endl;
+   else cout<<"NO"<<endl;
+
+}
+
+}
