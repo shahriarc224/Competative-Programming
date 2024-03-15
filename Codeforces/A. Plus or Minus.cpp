3a74d504fac62d6127b9233d8f350c222ade801e
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-15 11:35:43 +0600

    Create A. Plus or Minus.cpp

---
+++ Codeforces/A. Plus or Minus.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/contest/1807/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+
+while(t--){
+    int a[3];
+    for(int i=0;i<3;i++){
+        cin>>a[i];
+        }
+
+        if(a[0]+a[1]==a[2])cout<<"+"<<endl;
+           else if(a[0]-a[1]==a[2])cout<<"-"<<endl;
+
+
+
+}
+
+
+}
