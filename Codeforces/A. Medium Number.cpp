6043a3b9ad8586192de855ba165951a915d3b275
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-15 19:53:32 +0600

    Update A. Medium Number.cpp

--- Codeforces/A. Medium Number.cpp
+++ Codeforces/A. Medium Number.cpp
@@ -25,3 +25,27 @@ int main(){
     
   }
 }
+
+// or 
+
+/*
+
+//https://codeforces.com/contest/1760/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t; cin>>t;
+  while(t--){
+    vector<int>v(3);
+    for(int i=0;i<3;i++)cin>>v[i];
+    
+    sort(v.begin(),v.end());
+    cout<<v[1]<<endl;
+  }
+}
+
+
+
+*/
