676e9b543caf813fe0bba503a4e69184f8ff078e
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-15 17:31:37 +0600

    Create A. Odd One Out.cpp

---
+++ Codeforces/A. Odd One Out.cpp
@@ -0,0 +1,27 @@
+// https://codeforces.com/contest/1915/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+
+    int a[3];
+    for(int i=0;i<3;i++){
+        cin>>a[i];
+    }
+
+    if(a[0]==a[1]){
+    cout<<a[2]<<endl;
+}  if (a[1]==a[2]) {
+
+cout<<a[0]<<endl;
+
+}  if(a[0]==a[2]){
+  cout<<a[1]<<endl;
+
+}
+}
+}
