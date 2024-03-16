4525ec4177fd18e7b9179e1b76d3ca2e3bef0f84
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-16 19:23:39 +0600

    Create A. Watermelon.cpp

---
+++ Codeforces/A. Watermelon.cpp
@@ -0,0 +1,38 @@
+ /*
+
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                                Bismillahir Rahmanir Raheem
+                                      "In the name of God, Most Gracious, Most Merciful
+
+
+                Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+                llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+
+// https://codeforces.com/problemset/problem/4/A
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+int w;
+cin>>w;
+
+ if(w%2==0 && w>2){
+
+    cout<<"YES"<<endl;
+
+ }else{
+
+ cout<<"NO"<<endl;
+
+ }
+
+
+}
+ 
