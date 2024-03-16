2ee5604c0f9f525c02eabecea598c23311e27e12
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-16 10:54:16 +0600

    Create A. YES or YES?.cpp

---
+++ Codeforces/A. YES or YES?.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/contest/1703/problem/A
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  
+  int t;
+  cin>>t;
+  while(t--){
+  
+    string a;
+    cin>>a;
+    
+    if(a=="YES" || a=="YeS" || a=="YEs" || a=="yES" || a=="Yes" || a=="yEs" || a=="yeS" || a=="yes" || a=="YES" ){
+      cout<<"YES"<<endl;
+    } else cout<<"NO"<<endl;
+    
+    
+  }
+   
+}
