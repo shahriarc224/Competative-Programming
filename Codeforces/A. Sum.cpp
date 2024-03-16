25e2825615c0dd5038e4b13fb539e28edbf92876
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-16 08:25:28 +0600

    Create A. Sum.cpp

---
+++ Codeforces/A. Sum.cpp
@@ -0,0 +1,22 @@
+// https://codeforces.com/contest/1742/problem/A
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int a[3];
+        for (int i = 0; i < 3; i++) {
+            cin >> a[i];
+        }
+        if (a[0] + a[1] == a[2] || a[1] + a[2] == a[0] || a[2] + a[0] == a[1]) {
+            cout << "YES" << endl; 
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
