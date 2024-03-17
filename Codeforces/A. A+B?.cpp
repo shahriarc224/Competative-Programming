4ae7789723dc00f5e0cf1237a0ed960b2f18cf48
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-17 17:14:21 +0600

    Create A. A+B?.cpp

---
+++ Codeforces/A. A+B?.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/contest/1772/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin>> t;
+    while(t--){
+  string a;
+  cin>>a;
+ 
+ int j=a[0]-48;
+ int k=a[2]-48;
+ int l=j+k;
+ cout<<l<<endl;
+    }
+}
