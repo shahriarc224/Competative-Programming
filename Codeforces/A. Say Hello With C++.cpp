9dbe3542e90ec2d5eae24577cae638d854a89755
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-20 14:40:57 +0600

    Create A. Say Hello With C++.cpp

---
+++ Codeforces/A. Say Hello With C++.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  string a;
+  cin>>a;
+  
+    cout << "Hello, "<<a<<endl;
+    return 0;
+}
