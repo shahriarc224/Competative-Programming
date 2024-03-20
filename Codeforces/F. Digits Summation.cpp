a54aa311875a8409659dc130d87e972525f4dd29
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-20 16:20:22 +0600

    Create F. Digits Summation.cpp

---
+++ Codeforces/F. Digits Summation.cpp
@@ -0,0 +1,19 @@
+//  https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+  
+  string a,b;
+  cin>>a>>b;
+  
+  long c=a[a.size()-1]-'0';
+  long d=b[b.size()-1]-'0';
+  
+  
+  cout<<c+d<<endl;
+  
+    return 0;
+}
