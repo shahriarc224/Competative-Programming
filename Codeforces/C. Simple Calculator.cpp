573a149a91ea132e952f40f3265bfbade6a6d132
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-20 15:01:42 +0600

    Create C. Simple Calculator.cpp

---
+++ Codeforces/C. Simple Calculator.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  long long  a,b;
+  cin>>a>>b;
+  
+long long c =a+b;
+long long d=a-b;
+long long e=a*b;
+
+  
+  cout<<a<<" "<<"+ "<<b<<" "<<"= "<<c<<endl;
+   cout<<a<<" "<<"* "<<b<<" "<<"= "<<e<<endl;
+    cout<<a<<" "<<"- "<<b<<" "<<"= "<<d<<endl;
+    
+  
+  
+    return 0;
+}
