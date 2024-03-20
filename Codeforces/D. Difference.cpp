0c621f78a05040fe0c0e32ea249b112f4b995a3b
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-20 15:09:48 +0600

    Create D. Difference.cpp

---
+++ Codeforces/D. Difference.cpp
@@ -0,0 +1,15 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+  long long  a,b,c,d;
+  cin>>a>>b>>c>>d;
+  
+   long long e=(a*b)-(c*d);
+   cout<<"Difference = "<<e<<endl;
+
+    return 0;
+}
