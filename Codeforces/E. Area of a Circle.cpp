7e39f6a8131e4bfc23eda0e3a3a07b6743a3fbb9
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-20 15:13:29 +0600

    Create E. Area of a Circle.cpp

---
+++ Codeforces/E. Area of a Circle.cpp
@@ -0,0 +1,17 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+  double a;
+  cin>>a;
+  
+  double b= 3.141592653*a*a;
+  
+  
+  cout<<fixed<<setprecision(9)<<b<<endl;
+  
+    return 0;
+}
