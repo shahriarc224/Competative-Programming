10b69302821278bd2744b9b66d7e2aea1b1290bf
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-21 17:36:29 +0600

    Create H. Two numbers.cpp

---
+++ Codeforces/H. Two numbers.cpp
@@ -0,0 +1,17 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/H
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+  double a,b;
+  cin>>a>>b;
+  double x=a/b;
+
+ cout<<"floor "<<a<<" / "<<b<<" = "<<floor(x)<<endl;
+  cout<<"ceil "<<a<<" / "<<b<<" = "<<ceil(x)<<endl;
+  
+   cout<<"round "<<a<<" / "<<b<<" = "<<round(x)<<endl;
+  
+  
+}
