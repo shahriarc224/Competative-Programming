db8ff1964b23b7336c75007070ecd26d725000e6
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-22 11:43:36 +0600

    Create I. Welcome for you with Conditions.cpp

---
+++ Codeforces/I. Welcome for you with Conditions.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+  int a,b;
+  cin>>a>>b;
+  
+  cout<<(a>=b?"Yes":"No")<<endl;
+  
+  
+}
