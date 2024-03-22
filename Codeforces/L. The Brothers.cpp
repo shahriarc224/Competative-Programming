ae378bc85112b38a5fddba950115fb28997c9688
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-22 12:41:04 +0600

    Create L. The Brothers.cpp

---
+++ Codeforces/L. The Brothers.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/L
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+  string a,b,c,d;
+  cin>>a>>b>>c>>d;
+  
+  cout<<(b==d?"ARE Brothers":"NOT")<<endl;
+  
+return 0;
+}
