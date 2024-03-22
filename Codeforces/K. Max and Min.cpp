5d82298fa7a44f130a6188e5170e68c88ea814c2
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-22 12:17:31 +0600

    Create K. Max and Min.cpp

---
+++ Codeforces/K. Max and Min.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/K
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+int a[3];
+for(int i=0;i<=3;i++) cin>>a[i];
+
+sort(a,a+3);  
+  cout<<a[0]<<" "<<a[2]<<endl;
+  return 0;
+  
+}
