f1aea7d888c8c7546d302c6478613e423c82e159
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-22 20:03:08 +0600

    Create N. Char.cpp

---
+++ Codeforces/N. Char.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/N
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    char a;
+    cin>>a;
+    
+    if(a==tolower(a))  cout<<(char)toupper(a)<<endl;
+    else cout<<(char)tolower(a)<<endl;
+    
+}
