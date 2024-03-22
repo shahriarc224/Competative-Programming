0e0fb50d1ed38fe81384726207ef1375610d4d55
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-22 11:55:52 +0600

    Create J. Multiples.cpp

---
+++ Codeforces/J. Multiples.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/J
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+  int a,b;
+  cin>>a>>b;
+  
+  cout<<(a%b==0 ||b%a==0 ? "Multiples" : "No Multiples")<<endl;
+  
+  
+}
