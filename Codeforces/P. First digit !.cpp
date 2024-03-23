b39f471b37bfe56a9fc93e75a64a96653288e721
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-23 19:49:37 +0600

    Update P. First digit !.cpp

--- Codeforces/P. First digit !.cpp
+++ Codeforces/P. First digit !.cpp
@@ -8,7 +8,30 @@ int main(){
   cin>>a;
   
   cout<<(a[0]%2==0 ? "EVEN" : "ODD" )<<endl;
+}
+
+/*
+OR
+
+
+// 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+  string a;
+  cin>>a;
+  
+  int b=a[0]-'0';
+  
+  cout<<(b%2==0 ? "EVEN" : "ODD" )<<endl;
   
   
   
 }
+
+
+
+
+*/
