c5ef9585791b256fe6b62b6cfb062afab5087791
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-24 17:02:02 +0600

    Create R. Age in Days.cpp

---
+++ Codeforces/R. Age in Days.cpp
@@ -0,0 +1,25 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/R
+ 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+ long long a;
+ cin>>a;
+ 
+   
+   long long b=a/365;
+   long long c=a%365;
+   
+   long long e=c/30;
+   long long f=c%30;
+   
+   cout<<b<<" years"<<endl;
+   cout<<e<<" months"<<endl;
+   cout<<f<<" days"<<endl;
+   
+ 
+ 
+    
+  }
+  
