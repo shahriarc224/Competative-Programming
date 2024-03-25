6315941af76e8b68ade114eb8e979ece4c236595
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-25 15:36:50 +0600

    Create C. Next Alphabet.cpp

---
+++ Codeforces/C. Next Alphabet.cpp
@@ -0,0 +1,16 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/C
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ 
+ char a;
+ cin>>a;
+ 
+ char b=a+1;
+ 
+ if(a=='z') cout<<'a'<<endl;
+ else cout<<b<<endl;
+ 
+}
