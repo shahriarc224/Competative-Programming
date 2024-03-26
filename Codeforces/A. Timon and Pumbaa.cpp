feb93b4355b54e9c66d65a5f0d1bd570af9d54a6
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-26 11:34:45 +0600

    Create A. Timon and Pumbaa.cpp

---
+++ Codeforces/A. Timon and Pumbaa.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/A
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+  long long a,b;
+  cin>>a>>b;
+  
+  if(b>a)cout<<0<<endl;
+  else cout<<a-b<<endl;
+  
+}
