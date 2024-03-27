3b70fcf01076c98abe3eba73b51a6b9d1dcd29ef
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-27 16:31:57 +0600

    Create C. Replacement.cpp

---
+++ Codeforces/C. Replacement.cpp
@@ -0,0 +1,28 @@
+//  https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/C
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+  int t;
+  cin>>t;
+  while(t--){
+    long long a;
+    cin>>a;
+    if(a==0){
+      
+      cout<<0<<" ";
+    }
+    else if(a<0){
+      cout<<2<<" ";
+      
+    }else if(a>0){
+      
+      cout<<1<<" ";
+    }
+  
+  
+    
+  }
+  
+}
