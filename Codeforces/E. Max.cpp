24e50d58d80040f002080631c2b79f9ba9960f4d
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-27 16:38:07 +0600

    Create  E. Max.cpp

---
+++ Codeforces/E. Max.cpp
@@ -0,0 +1,22 @@
+//  https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/E
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+ int t;
+ cin>>t;
+ long long a[t];
+   
+ for(int i=0;i<t;i++){
+   cin>>a[i];
+   
+   
+ }  
+   sort(a,a+t);
+   
+   cout<<a[t-1]<<endl;
+   
+ 
+}
+
