3372aed8aa5d6dcc8069556514834669f823a19b
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-27 16:33:28 +0600

    Create D. Positions in array.cpp

---
+++ Codeforces/D. Positions in array.cpp
@@ -0,0 +1,23 @@
+//  https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/D
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+  int t;
+  cin>>t;
+  while(t--){
+    
+    long long a;
+    cin>>a;
+    
+    long long b[a];
+    int cnt=0;
+    if(a<=10){
+     int x=0;
+     x++;
+     
+     cout<<"A["<<x<<"] = "<<a<<endl;
+     }
+  }
+}
