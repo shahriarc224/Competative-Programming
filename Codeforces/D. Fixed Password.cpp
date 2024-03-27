af8598fc0fa14804537827438d45a4139bd6a537
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-27 16:37:38 +0600

    Create D. Fixed Password.cpp

---
+++ Codeforces/D. Fixed Password.cpp
@@ -0,0 +1,26 @@
+
+//  https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+  while(1){
+  
+  string s;
+  cin>>s;
+  
+//long long a=s-'O'
+  
+  if(s=="1999"){
+   
+   cout<<"Correct"<<endl;
+   
+    break ;
+  }else {
+    
+    cout<<"Wrong"<<endl;
+  }
+  
+  }
+}
