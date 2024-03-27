bd0453941993623d0f84f6e06c41a484b88e83de
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-27 16:38:52 +0600

    Create F. Multiplication table.cpp

---
+++ Codeforces/F. Multiplication table.cpp
@@ -0,0 +1,17 @@
+//  https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/F
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+ long long a;
+ cin>>a;
+ 
+ for(int i=1;i<=12;i++){
+   
+  cout<<a<<" * "<<i<<" = "<<a*i<<endl; 
+ }
+   
+ 
+}
+
