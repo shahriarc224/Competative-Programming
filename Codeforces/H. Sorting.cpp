73153dcc3269c74cf593929761f492943a99a5de
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-28 19:24:32 +0600

    Create H. Sorting.cpp

---
+++ Codeforces/H. Sorting.cpp
@@ -0,0 +1,28 @@
+//  https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/H
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+long long a;
+cin>>a;
+
+long long b[a];
+for(int i=0;i<a;i++){
+  
+  cin>>b[i];
+  
+  
+}
+
+sort(b,b+a);
+
+for(int i=0;i<a;i++){
+  
+  cout<<b[i]<<" ";
+  
+  
+}
+}
+ 
