746a992e651a8dd342183cb29c2d49ec4a610e68
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-28 19:16:40 +0600

    Create F. Reversing.cpp

---
+++ Codeforces/F. Reversing.cpp
@@ -0,0 +1,24 @@
+//  https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/F
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+long long a;
+cin>>a;
+
+long long b[a];
+
+for(int i=0;i<a;i++){
+  cin>>b[i];
+  
+}
+
+for(int i=a-1;i>=0;i--){
+  cout<<b[i]<<" ";
+  
+}
+
+}
+ 
