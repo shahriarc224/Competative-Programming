45e5f9d845a67342c83d4268f2fed871cf98c3c5
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-03-29 10:17:29 +0600

    Create E. Count.cpp

---
+++ Codeforces/E. Count.cpp
@@ -0,0 +1,16 @@
+//  https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/E
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+string a;
+cin>>a;
+
+long long sum = 0;
+    for (int i = 0; i < a.size(); i++) {
+        sum =sum+ (a[i] - '0');  
+    }
+
+    cout << sum << endl;
+}
