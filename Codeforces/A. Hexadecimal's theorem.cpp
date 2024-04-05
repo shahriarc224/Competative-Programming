40bca281a764fce2f3a89b594691110c4bd6398b
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-04-05 23:35:15 +0600

    Create A. Hexadecimal's theorem.cpp

---
+++ Codeforces/A. Hexadecimal's theorem.cpp
@@ -0,0 +1,12 @@
+//  https://codeforces.com/problemset/problem/199/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+  long long a;
+  cin>>a;
+  
+  cout<<"0 "<<"0 "<<a<<endl;
+  
+}
