42ef77eecafb5e1d066d4e1af675d83483a889ff
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-04-14 21:57:51 +0600

    Create B. Upscaling.cpp

---
+++ Codeforces/B. Upscaling.cpp
@@ -0,0 +1,27 @@
+//  https://codeforces.com/contest/1950/problem/B
+
+#include <bits/stdc++.h>
+        using namespace std;
+     
+        int main() {
+        	int t;
+        	cin>>t;
+        	while (t--){
+        		int n;cin>>n;
+        		for(int i=0;i<2*n;i++){
+     
+     
+        			for(int j=0;j<2*n;j++){
+        				if((i/2+j/2)%2==0){
+        					cout<<'#';
+        				}
+        				else cout<<'.';
+        				
+        				
+        				
+        			}
+        			cout<<endl;
+        		}
+     
+        	}
+        }
