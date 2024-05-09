90db753472ccfda080643da51a8ef88fad9c1871
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-05-09 21:03:25 +0600

    Create H. Senior Junior can't stay together.cpp

---
+++ Codeforces/H. Senior Junior can't stay together.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/gym/523071/problem/H
+
+#include<bits/stdc++.h>
+    using namespace std;
+     
+    int main() 
+    {
+      
+     int a;
+     cin>>a;
+     
+     if(a==48 || a==49 || a==50 ){
+       cout<<"Probin"<<endl;
+       
+     }else if (a==51 || a==52 || a==53){
+       cout<<"Nobin"<<endl;
+     } else cout<<"Bohiragoto"<<endl;
+     
+        return 0;
+    }
