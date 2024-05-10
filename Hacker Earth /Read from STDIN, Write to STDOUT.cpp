91757a3f3f89818add44a909820d8f2f750995bb
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-05-10 11:36:32 +0600

    Create Read from STDIN, Write to STDOUT.cpp

---
+++ Hacker Earth /Read from STDIN, Write to STDOUT.cpp
@@ -0,0 +1,14 @@
+//  https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/io-exercise/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int n;cin>>n;
+string s;cin>>s;
+
+cout<<n*2<<endl;
+cout<<s<<endl;
+
+}
+
