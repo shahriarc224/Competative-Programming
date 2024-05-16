16fa3156dd344f43631550004f7993964a661f81
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-05-16 23:21:28 +0600

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,37 @@
+
+/*                                 			 
+ ********************************************( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )*********************************************
+                                            	Bismillahir Rahmanir Raheem
+                                 	"In the name of God, Most Gracious, Most Merciful
+              			 
+              			 
+  			  Allahumma anta rabbi la ilaha illa anta, ‘alaika tawakkaltu wa anta rabbul ‘arshil kareem maashaa
+  			  llahu kaana wa maLam yasha’ lam yakun-wa laa hawla wa laa quwwata illaa billaahil ‘aliyyil ‘azeem
+
+
+*/
+// 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dl;
+
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+int main(){
+
+   optimize ();
+
+
+
+
+return 0;
+
+}
+
+
+
