3ea4af4966432768bdf282a8f6937ab4b11e6e78
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-05-31 15:16:04 +0600

    Update BeeCrowd 1009.cpp

--- BeeCrowd/BeeCrowd 1009.cpp
+++ BeeCrowd/BeeCrowd 1009.cpp
@@ -6,13 +6,10 @@ using namespace std;
 int main(){
     string a;
     cin>>a;
-
     double b,c;
     cin>>b>>c;
     double total =(((15*c)/100)+b);
     
     cout<<"TOTAL = R$ "<<fixed<<setprecision(2)<<total<<endl;
-    
-    
     return 0;
 }
