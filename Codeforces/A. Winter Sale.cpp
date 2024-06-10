1396bcad5439b84f13f538ef632b252ba6cbdc47
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-06-10 19:25:57 +0600

    Create A. Winter Sale.cpp

---
+++ Codeforces/A. Winter Sale.cpp
@@ -0,0 +1,15 @@
+    // https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/A
+     
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main(){
+      
+      float a,b;
+      cin>>a>>b;
+      
+    float c=(b*100)/(100-a);
+      
+      cout<<fixed<<setprecision(2)<<c<<endl;
+      
+      return 0;
+    }
