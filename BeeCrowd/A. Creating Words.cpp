3c6e9909a5a15f03886f740c5a916a8a63918f4d
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-06-11 22:34:57 +0600

    Create A. Creating Words.cpp

---
+++ BeeCrowd/A. Creating Words.cpp
@@ -0,0 +1,12 @@
+    //  https://codeforces.com/contest/1985/problem/A
+
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main(){
+    	int t; cin>>t;
+    	while(t--){
+    		string a,b;cin>>a>>b;
+    		cout<<b[0]<<a[1]<<a[2]<<" "<<a[0]<<b[1]<<b[2]<<endl;	
+    	}
+    	return 0;
+    }
