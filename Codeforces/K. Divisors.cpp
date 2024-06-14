eebecf94eae97bf130505a327260cf683b061f46
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-06-14 20:02:18 +0600

    Create K. Divisors.cpp

---
+++ Codeforces/K. Divisors.cpp
@@ -0,0 +1,14 @@
+    //  https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/K
+     
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main(){
+      
+      int a;cin>>a;
+      
+      for(int i=1;i<=a;i++){
+        if(a%i==0) cout<<i<<endl;
+        
+      }
+      return 0;
+    }
