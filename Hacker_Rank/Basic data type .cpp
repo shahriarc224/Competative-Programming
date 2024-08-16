0b18b6d478b7fa8ccd365195d73b0b47b973abed
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-08-16 19:41:50 +0600

    Create Basic data type .cpp

---
+++ Hacker_Rank/Basic data type .cpp
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
+
+#include<bits/stdc++.h>
+using namespace std ;
+int main(){
+    int a;cin>>a;
+    
+    long int b; cin>>b;
+    
+    char c; cin>>c;
+    
+    float d; cin>>d;
+    
+    double e; cin>>e;
+    
+        cout<<a<<endl;
+        cout<<b<<endl;
+      cout<<c<<endl;
+    cout<<fixed<<setprecision(3)<<d<<endl;
+     cout<<fixed<<setprecision(9)<<e<<endl;
+
+
+    
+    return 0;
+}
