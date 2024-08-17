1d934d9c160aaf98f32474ccda3b98f396013547
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-08-17 18:29:17 +0600

    Create Pointer .cpp

---
+++ Hacker_Rank/Pointer .cpp
@@ -0,0 +1,12 @@
+// https://www.hackerrank.com/challenges/c-tutorial-pointer/problem?isFullScreen=true
+
+#include<bits/stdc++.h>
+using namespace std ;
+ int main(){
+     
+     int a,b; cin>>a>>b;
+     
+     cout<<a+b<<endl;
+     if(a>b) cout<<a-b<<endl;
+     else cout<<b-a<<endl;
+ }
