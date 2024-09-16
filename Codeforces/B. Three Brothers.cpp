261145bd33ff4bbce0f0c76191519189b932d49b
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-09-16 19:53:24 +0600

    Create B. Three Brothers.cpp

---
+++ Codeforces/B. Three Brothers.cpp
@@ -0,0 +1,15 @@
+// https://codeforces.com/problemset/problem/2010/B
+
+#include<bits/stdc++.h>
+using namespace std ;
+int main(){
+	
+	int a,b; cin>>a>>b;
+	if(a==1 && b==2) cout<<"3"<<endl;
+	if(a==1 && b==3) cout<<"2"<<endl;
+	if(a==2 && b==1) cout<<"3"<<endl;
+	if(a==2 && b==3) cout<<"1"<<endl;
+	if(a==3 && b==1) cout<<"2"<<endl;
+	if(a==3 && b==2) cout<<"1"<<endl;
+	return 0;
+}
