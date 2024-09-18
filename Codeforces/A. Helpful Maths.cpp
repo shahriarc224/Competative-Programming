cceeb2e079bae1e0a9252218fc177ebdcc63522b
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-09-18 19:29:45 +0600

    Create A. Helpful Maths.cpp

---
+++ Codeforces/A. Helpful Maths.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/problemset/problem/339/A
+
+#include<bits/stdc++.h>
+using namespace std ;
+int main(){
+ string s; cin>>s;
+
+  vector<int>v;
+   for(int i=0;i<s.size();i+=2){
+     v.push_back(s[i]-'0');
+  }
+  sort(v.begin(),v.end());
+    for(int i=0;i<v.size();i++){
+       cout<<v[i];
+       if(i+1!=v.size()) cout<<"+"; 
+       else cout<<" ";
+    }
+         	return 0;
+        }
