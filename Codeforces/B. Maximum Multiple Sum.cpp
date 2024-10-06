219786cc5ab4da109911f7c90a7929c0cb5e1e48
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-10-06 10:22:22 +0600

    Create B. Maximum Multiple Sum.cpp

---
+++ Codeforces/B. Maximum Multiple Sum.cpp
@@ -0,0 +1,17 @@
+// https://codeforces.com/contest/1985/problem/B
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   int t; cin>>t;
+   
+   while(t--){
+     int n; cin>>n;
+     if(n==3) cout<<"3"<<endl;
+   else cout<<"2"<<endl;
+  
+   }
+}   
+
