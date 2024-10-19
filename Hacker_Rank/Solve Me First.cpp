d9f3671c770c16456c51c43bb3d4f0c5f9b4f377
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2024-10-19 20:00:52 +0600

    Create Solve Me First.cpp

---
+++ Hacker_Rank/Solve Me First.cpp
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/solve-me-first/problem
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve (){
+    int a,b; cin>>a>>b;
+
+cout<<a+b<<endl;
+}
+
+int main(){
+    
+    solve();
+
+    return 0;
+}
