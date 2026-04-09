d3f253a02b64a38ac35af6c75b7278b3ce900ff5
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2026-04-09 22:40:32 +0600

    Update Two Sum.cpp

--- LeetCode/Two Sum.cpp
+++ LeetCode/Two Sum.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
         for(int i=0;i<nums.size();i++){
-            
-           for(int j=1;j<nums.size();j++){
-               
+           for(int j=1;j<nums.size();j++){ 
               if(nums[i]+nums[j]==target )
                   return {i,j};
            }
