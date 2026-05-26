8f132ede5f58c9e6b98f7fe833dbc6fa0be01004
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2026-05-26 23:36:42 +0600

    Update canMakeArithmeticProgression.cpp

--- LeetCode/canMakeArithmeticProgression.cpp
+++ LeetCode/canMakeArithmeticProgression.cpp
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
 class Solution {
 public:
+
     bool canMakeArithmeticProgression(vector<int>& arr) {
         sort(arr.begin(),arr.end());
         int d=arr[1]-arr[0];
