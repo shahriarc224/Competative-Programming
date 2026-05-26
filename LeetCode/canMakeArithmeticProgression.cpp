1e7e12878b25d26d3e6ab079d79f468415a93ccf
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2026-05-26 23:36:29 +0600

    Update canMakeArithmeticProgression.cpp

--- LeetCode/canMakeArithmeticProgression.cpp
+++ LeetCode/canMakeArithmeticProgression.cpp
@@ -6,7 +6,9 @@ public:
         int d=arr[1]-arr[0];
         for(int i=2;i<arr.size();i++){
             if(arr[i]-arr[i-1]!= d) return false;
+            
        }
+        
        return true;
     }
     
