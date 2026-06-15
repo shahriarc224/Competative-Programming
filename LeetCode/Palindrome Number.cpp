36e86c34066414dd1c3edcb297557095b7897932
Md. Shahriar Chowdhury <145952063+shahriarc224@users.noreply.github.com>
2026-06-16 00:13:30 +0600

    Update Palindrome Number.cpp

--- LeetCode/Palindrome Number.cpp
+++ LeetCode/Palindrome Number.cpp
@@ -2,14 +2,6 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         string a =to_string(x);]\
-
-
-
-
-
-        
-
-
         
         string dup=a;
           reverse(dup.begin(),dup.end());
