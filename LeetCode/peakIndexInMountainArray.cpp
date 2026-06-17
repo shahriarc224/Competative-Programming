fd177179fc7d2b011b6c20393d86551ad0596735
Md. Shahriar Chowdhury <145952063+shahriarc224@users.noreply.github.com>
2026-06-17 16:53:21 +0600

    Update peakIndexInMountainArray.cpp

--- LeetCode/peakIndexInMountainArray.cpp
+++ LeetCode/peakIndexInMountainArray.cpp
@@ -2,6 +2,15 @@
 
 class Solution {
 public:
+
+
+
+
+
+
+
+
+
     int peakIndexInMountainArray(vector<int>& arr) {
         int t=max_element(arr.begin(),arr.end())-arr.begin();
         return t;
