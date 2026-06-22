feb43e67485f595cdad5ce49dd4a8280b3c9235d
Md. Shahriar Chowdhury <145952063+shahriarc224@users.noreply.github.com>
2026-06-23 00:05:10 +0600

    Update Plus Minus.cpp

--- Hacker_Rank/Plus Minus.cpp
+++ Hacker_Rank/Plus Minus.cpp
@@ -21,6 +21,10 @@ void solve(){
         pos=pos/n;
          neg=neg/n;
           zero=zero/n;
+
+
+
+
     
     cout<<fixed<<setprecision(6)<<pos<<endl;
      cout<<fixed<<setprecision(6)<<neg<<endl;
