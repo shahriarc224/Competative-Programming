647f4194241bb0c9d2153182bd04f4ce820abeae
Md. Shahriar Chowdhury <145952063+shahriarc224@users.noreply.github.com>
2026-06-25 20:17:04 +0600

    Update canMakeArithmeticProgression.cpp

--- LeetCode/canMakeArithmeticProgression.cpp
+++ LeetCode/canMakeArithmeticProgression.cpp
@@ -6,8 +6,6 @@ public:
         sort(arr.begin(),arr.end());
         int d=arr[1]-arr[0];
         for(int i=2;i<arr.size();i++){
-
-            
             if(arr[i]-arr[i-1]!= d) return false;
             
        }
