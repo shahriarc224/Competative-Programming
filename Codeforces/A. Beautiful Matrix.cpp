d98c09d921513478d6506d747e1803eb04d4ceed
Md. Shahriar Chowdhury <145952063+shahriarc224@users.noreply.github.com>
2026-06-27 23:14:08 +0600

    Update A. Beautiful Matrix.cpp

--- Codeforces/A. Beautiful Matrix.cpp
+++ Codeforces/A. Beautiful Matrix.cpp
@@ -22,9 +22,6 @@ typedef double dl;
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
 
-
-
-
 int main(){
 	
    optimize ();
