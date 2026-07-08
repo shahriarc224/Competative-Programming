9522c0d9540dbeb81daa251eb91e0fb8bed79195
Md. Shahriar Chowdhury <145952063+shahriarc224@users.noreply.github.com>
2026-07-08 23:02:43 +0600

    Update Two Sum.cpp

--- LeetCode/Two Sum.cpp
+++ LeetCode/Two Sum.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<nums.size();i++){
            for(int j=1;j<nums.size();j++){ 
               if(nums[i]+nums[j]==target )
-                  
+                  \\dfg
                   return {i,j};
                
            }
