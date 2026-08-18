ef424d274bea9e302e0aa36d2876e7346f68991f
Md. Shahriar Chowdhury <145952063+shahriarc224@users.noreply.github.com>
2026-08-18 22:13:36 +0600

    Update A. Team.cpp

--- Codeforces/A. Team.cpp
+++ Codeforces/A. Team.cpp
@@ -7,6 +7,7 @@ int main(){
 int t; cin>>t;
     int arr[3];
     int cnt=0;
+    
     while(t--){
         for(int i=0;i<3;i++) cin>>arr[i];
         if(arr[0]== 1 && arr[1]== 1 && arr[2]== 1 || arr[0]== 1 && arr[1]== 1 && arr[2]== 0 || arr[0]== 1 && arr[1]== 0 && arr[2]== 1 || arr[0]== 0 && arr[1]== 1 && arr[2]== 1){
